def8473efc9b1f4b6c2e70b461a009c25ba3b3bb
IanCKW <iankwchan@gmail.com>
2023-09-22 21:27:46 +0800

    add sgcr link to logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,13 @@ const Header: React.FC<HeaderProps> = () => {
   return (
     <header>
       <div id="inner_top">
-        <img id="sgcrLogo" src="/sgcrLogo.png" alt="Logo" />
+        <a
+          href="https://www.sgclimaterally.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img id="sgcrLogo" src="/sgcrLogo.png" alt="Logo" />
+        </a>
       </div>
       <h1 data-text="Email your MPs">Email your MPs</h1>
     </header>
